6e86e1aca6fd65861add6dd447522dfb9d814043
sohelhussain <sohelhussaing@gmail.com>
2025-02-14 22:45:36 +0530

    feat: ADDING FORM

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useNavigate } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import Landing from "./page/Landing";
 import Identify from "./page/Identify";
 
