05029745f6bb928b306e2a1fbb4756c5c4aa2d97
sohelhussain <sohelhussaing@gmail.com>
2025-02-14 19:04:13 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+<div className="w-full h-screen bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-padding backdrop-filter backdrop-blur-md bg-white/10">
+    
+</div>
+  )
+}
+
+export default App
